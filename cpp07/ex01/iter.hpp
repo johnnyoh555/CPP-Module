bc4d2d742e4fbbcf88e68c130c04828557d3dedc
jooh <johnn55@naver.com>
2024-04-04 15:11:16 +0900

    fix: cpp07 try2

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -8,9 +8,4 @@ void	iter(T arr[], const size_t length, const F func) {
 	for (size_t i = 0; i < length; i++) func(arr[i]);
 }
 
-template<typename T>
-void	iter(T arr[], const size_t length, const T func) {
-	for (size_t i = 0; i < length; i++) arr[i] = func(arr[i]);
-}
-
 #endif
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -1,13 +1,13 @@
 #include "iter.hpp"
 
-char uppercase(char c) {
+void uppercase(char c) {
 	if (c >= 'a' && c <= 'z')
-		return c + ('A' - 'a');
-	return c;
+		c = c + ('A' - 'a');
+	std::cout << c;
 }
 
-int	plusone(int i) {
-	return i + 1;
+void	plusone(int i) {
+	std::cout << i + 1;
 }
 
 int main(void) {
@@ -15,15 +15,7 @@ int main(void) {
 	char str[6] = {"Hello"};
 
 	::iter(arr, 10, plusone);
+	std::cout << '\n';
 	::iter(str, 6, uppercase);
-
-	for (int i = 0; i < 10; i++) {
-		std::cout << arr[i] << ' ';
-	}
 	std::cout << '\n';
-	for (int i = 0; i < 6; i++) {
-		std::cout << str[i];
-	}
-	std::cout << '\n';
-
 }
