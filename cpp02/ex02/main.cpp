d746b805b3d07ae4ee27ae6b80763de91f18b2a3
jooh <johnn55@naver.com>
2024-04-04 15:00:36 +0900

    fix:

--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -5,7 +5,9 @@
 int main(void) {
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	Fixed const c(Fixed(-1.5f));
 	
+	std::cout << c.toInt() << std::endl;
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;
